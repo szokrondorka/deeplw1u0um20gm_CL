dbab2a505657eb6b26798a2e7069c097fc81e488
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-11-23 21:15:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import torch 
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-
 
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
