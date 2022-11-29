cf2cc30e9ab0af705cd43bc42a37601ba89f80df
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-11-29 23:35:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,29 +22,8 @@ model = ConvNet(num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-train_loader = [train_loader0,train_loader1,train_loader2,train_loader3,train_loader4]
-test_loader = [test_loader0,test_loader1,test_loader2,test_loader3,test_loader4]
-
 # Train the model
-for i in range(0,5):
-  total_step = len(train_loader[i])
-  for epoch in range(num_epochs):
-      for j, (images, labels) in enumerate(train_loader[i]):
-          images = images.to(device)
-          labels = labels.to(device)
-          
-          # Forward pass
-          outputs = model(images)
-          loss = criterion(outputs, labels)
-          
-          # Backward and optimize
-          optimizer.zero_grad()
-          loss.backward()
-          optimizer.step()
-          
-          if (j+1) % 100 == 0:
-              print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+# train.py
 
 # Test the model
 model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
