e99922c82fa98c97437ce6f8808e4c2d82084577
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-11-29 23:43:33 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,24 @@
 
 
 # Train the model
-for i in range(0,5):
+
+class trainTutorial():
+  
   total_step = len(train_loader[i])
   for epoch in range(num_epochs):
       for j, (images, labels) in enumerate(train_loader[i]):
           images = images.to(device)
           labels = labels.to(device)
-          
+
           # Forward pass
           outputs = model(images)
           loss = criterion(outputs, labels)
-          
+
           # Backward and optimize
           optimizer.zero_grad()
           loss.backward()
           optimizer.step()
-          
+
           if (j+1) % 100 == 0:
               print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                     .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
