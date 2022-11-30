7db25c75c266f0dd6532e631f6e9bdbad01929ed
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-11-30 01:21:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 # Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Hyper parameters
 num_epochs = 5
@@ -19,26 +19,52 @@ learning_rate = 0.001
 model = ConvNet(num_classes).to(device)
 
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
 # Train the model
-# train.py
-
 # Test the model
-model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+# train.py
 
 # Save the model checkpoint
 #torch.save(model.state_dict(), 'model.ckpt')
+
+
+class ExperimentManager():
+    def __init__(self, no_cuda=False, num_workers=2, logdir=None, prefix='',
+                 datadir=os.path.expanduser('~/datasets')):
+        self.logdir = logdir
+        self.prefix = prefix
+        self.datadir = datadir
+
+        self.setup_environment()
+        self.setup_torch()
+        self.setup_trainer()
+
+    def setup_environment(self):
+        os.makedirs(self.datadir, exist_ok=True)
+
+        if self.logdir is not None:
+            self.logdir = os.path.join(self.logdir, self.prefix)
+            os.makedirs(self.logdir, exist_ok=True)
+
+    def setup_torch(self):
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')   
+        print("Device: {}".format(self.device))
+
+    def setup_trainer(self):
+        self.data = data.Data(self.datadir,
+                              self.dataloader_kwargs)
+
+        self.model = models.Model(self.device,
+                                  self.data.input_shape,
+                                  self.data.num_classes)
+
+        self.trainer = trainers.trainer_maker(self.data.target_type,
+                                              self.device,
+                                              self.model.build(),
+                                              self.data.batch_size,
+                                              self.data.num_tasks,
+                                              self.data.num_cycles,
+                                              self.data.loaders,
+                                              self.logdir)
+
+    def run_experiment(self):
+        self.trainer.train()
