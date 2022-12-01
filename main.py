0344630c4c89e6b5478aecefeb3facca191567aa
szokrondorka <71253642+szokrondorka@users.noreply.github.com>
2022-12-01 14:44:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+import torch 
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+import data
+import models
+
+# Device configuration
+if torch.cuda.is_available() is False:
+    raise Exception("GPU device not found, runtime environment should be set to GPU")
+print(f"Using GPU device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+# Data loader
+# data.py
+
+# Convolutional neural network (two convolutional layers)
+# models.py
+
+model = models.ConvNet(num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for i in range(0,5):
+  total_step = len(train_loader[i])
+  for epoch in range(num_epochs):
+      for j, (images, labels) in enumerate(train_loader[i]):
+          images = images.to(device)
+          labels = labels.to(device)
+          
+          # Forward pass
+          outputs = model(images)
+          loss = criterion(outputs, labels)
+          
+          # Backward and optimize
+          optimizer.zero_grad()
+          loss.backward()
+          optimizer.step()
+          
+          if (j+1) % 100 == 0:
+              print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+# Test the model
+model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+#torch.save(model.state_dict(), 'model.ckpt')
